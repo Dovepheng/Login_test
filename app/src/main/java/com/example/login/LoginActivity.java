96f75a9014b9bb18ed15a57e94385fa2798257d1
lxs137 <lxs137@hotmail.com>
2016-04-23 16:59:30 +0800

    login activity

--- app/src/main/java/com/example/login/LoginActivity.java
+++ app/src/main/java/com/example/login/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Window;
 
 public class LoginActivity extends AppCompatActivity {
@@ -10,7 +11,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.login_layout);
+
     }
 }
